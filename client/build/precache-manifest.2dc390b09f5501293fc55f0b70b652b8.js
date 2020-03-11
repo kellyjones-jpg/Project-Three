31830372bb26ed5f723f027b0f22e895f33613e3
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2020-03-10 21:21:31 -0600

    removed yarn

---
+++ client/build/precache-manifest.2dc390b09f5501293fc55f0b70b652b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48fe2caab3d9d2ce90be0db147704686",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca7aa214b8ad957026c1",
+    "url": "/static/css/2.0996cbbd.chunk.css"
+  },
+  {
+    "revision": "24b0a7c8757af0498587",
+    "url": "/static/css/main.af1e3b54.chunk.css"
+  },
+  {
+    "revision": "ca7aa214b8ad957026c1",
+    "url": "/static/js/2.8be8cf10.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.8be8cf10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24b0a7c8757af0498587",
+    "url": "/static/js/main.683744f6.chunk.js"
+  },
+  {
+    "revision": "226fd4a5de4b477d8cce",
+    "url": "/static/js/runtime-main.6ff082a3.js"
+  }
+]);
