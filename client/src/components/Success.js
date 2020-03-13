bd7bd131924d861552e42f39cef7ec6f2877f934
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2020-03-13 14:59:34 -0600

    finished success file

--- client/src/components/Success.js
+++ client/src/components/Success.js
@@ -1,6 +1,14 @@
 import React from "react";
-import { Button, Form, FormGroup, Label, Input, FormText, Col } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText, Col, Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Row } from 'reactstrap';
 import axios from 'axios';
+import { Media } from 'reactstrap';
+import Logo from '../components/assets/Logo.png';
+
+var imgStyle = {
+  // minWidth: "28px",
+  // paddingLeft: "60%",
+};
 
 class Success extends React.Component {
     state = {
@@ -10,9 +18,38 @@ class Success extends React.Component {
 
     render() {
         return (
-            <div className="login">
+            <div className="login" style={{paddingTop: "3%"}}>
               <container>
-         <h2>You registered!</h2>
+              <FormGroup row>
+              <Col sm="12" md={{ size: 6, offset: 3 }}>
+                <div style={{textAlign: "center", color:"black"}}><h1>W E L C O M E</h1></div>
+                </Col>
+                </FormGroup>
+                <FormGroup row>
+              <Col sm="12" md={{ size: 6, offset: 3 }}>
+              <div style={{textAlign: "center", color:"black"}}><p>TO</p></div>
+                </Col>
+                </FormGroup>
+                <Row>
+                <Col sm="12" md={{ size: 7, offset: 4 }}>
+      <div style={{paddingLeft: "4%", paddingTop: ".25%"}}><img src={Logo} alt="Logo" /></div>
+    </Col>
+  </Row>
+                {/* <Media row>
+      <Media >
+      <Col sm="12" md={{ size: 1, offset: 11 }}>
+      <div className="float-left"><Media style={imgStyle} object src={Logo} alt="Logo" /></div>
+        </Col>
+      </Media>
+      </Media> */}
+      <row>
+        <div style={{paddingTop: "1.5%"}}>
+      <Col sm="12" md={{ size: 6, offset: 3 }}>
+      <div className="text-center"><Button color="secondary" href={"/"}>BACK TO LOGIN</Button></div>
+      </Col>
+      </div>
+      </row>
+  
     </container>
             </div>
         );
